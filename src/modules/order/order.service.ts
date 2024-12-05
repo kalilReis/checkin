2f37738300ce99e6366e191dd9ab70494d68ab5b
kalilReis <kalilreisdesisto@gmail.com>
2024-12-05 17:30:59 -0300

    consuming wc api

--- src/modules/order/order.service.ts
+++ src/modules/order/order.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 
 @Injectable()
 export class OrderService {
-  get() {
-    return 1;
+  async get() {
+    const { STORE_PRODUCTS_URL, CONSUMER_KEY, CONSUMER_SECRET } = process.env;
+
+    const headers = {
+      Authorization: 'Basic ' + btoa(`${CONSUMER_KEY}:${CONSUMER_SECRET}`),
+    };
+
+    const resp = await axios.get(STORE_PRODUCTS_URL ?? '', { headers });
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return resp.data;
   }
 }
